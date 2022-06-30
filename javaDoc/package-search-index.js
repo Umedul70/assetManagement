b934981778e5ad14c2c2c43a4e30751c103b9ddc
Umedul70 <90558151+Umedul70@users.noreply.github.com>
2022-07-01 01:50:14 +0530

    Add files via upload

---
+++ javaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.assetManagment.assetManagement"},{"l":"com.assetManagment.assetManagement.controller"},{"l":"com.assetManagment.assetManagement.dao"},{"l":"com.assetManagment.assetManagement.entities"},{"l":"com.assetManagment.assetManagement.services"}];updateSearchResults();
